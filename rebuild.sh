d89b4622fcfdda1836e9af89b14b13053657afcc
Jim Miller <millerjv@ge.com>
2017-10-11 11:37:02 -0400

    Updaing messages with proper store names

--- rebuild.sh
+++ rebuild.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 echo ""
-echo "Removing DICOM Store image."
+echo "Removing datastore image."
 docker rmi rt106/rt106-datastore:latest
 
 echo ""
-echo "Building DICOM Store image."
+echo "Building datastore image."
 docker build -t rt106/rt106-datastore:latest --build-arg http_proxy=$HTTP_PROXY --build-arg https_proxy=$HTTPS_PROXY .
 
 echo ""
